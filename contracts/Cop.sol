945f07babb8da34b1211e5ae57ffc996397ef28f
lnrdmnc <l.monaco11@studenti.unisa.it>
2023-02-10 23:53:15 +0100

    add smart contract in solidity Cop that allow to buy a sneakers and migrate

---
+++ contracts/Cop.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+contract Cop {
+
+address[16] public sneakers;
+// cop a sneakers
+function cop(uint snkrsId) public returns (uint) {
+  require(snkrsId >= 0 && snkrsId <= 15);
+
+  sneakers[snkrsId] = msg.sender;
+
+  return snkrsId;
+}
+
+// Recupera tutte le sneakers
+function getSneakers() public view returns (address[16] memory) {
+  return sneakers;
+}
+
+
+}
